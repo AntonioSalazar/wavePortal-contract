bb998cc9ea8d065658ad0f876a685a8d48aa3ff3
Antonio Salazar <antonio.salazar@wizeline.com>
2022-01-17 11:32:43 -0600

    saving smartcontract to github

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+
+    uint256 totalWaves;
+    // used to generate a random number
+    uint256 private seed;
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+
+    mapping(address => uint256) public lastWavedAt;
+    constructor() payable {
+        console.log("Howdy there cowboy!!");
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _message) public {
+
+        require(lastWavedAt[msg.sender] + 15 minutes < block.timestamp, "Wait 15 minutes");
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
+        totalWaves += 1;
+        console.log("%s has waved! ", msg.sender);
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        seed = (block.timestamp + block.difficulty) % 100;
+        console.log("Random # generated: %d", seed);
+        // gives a 50% chance that the user wins the prize
+        if(seed <=50){
+            console.log("%s won!", msg.sender);
+            uint256 prizeAmount = 0.0001 ether;
+            require(prizeAmount <= address(this).balance, "Trying to withdraw more money that the contract has");
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns(Wave[] memory){
+        return waves;
+    }
+
+    function getTotalWaves () public view returns(uint256){
+        console.log("We have %d total waves! ", totalWaves);
+        return totalWaves;
+    }
+
+
+}
